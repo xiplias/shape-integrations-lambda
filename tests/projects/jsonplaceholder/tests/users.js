932770862632d43337adbf113520ba5754953cd2
Anders Hansen <xiplias@gmail.com>
2017-06-14 13:39:14 +0200

    Update core to 0.0.8 and update tests

--- tests/projects/jsonplaceholder/tests/users.js
+++ tests/projects/jsonplaceholder/tests/users.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: 'GET users',
   description: 'Tests getting the users',
-  testFunction: function(context, logger, callback) {
-    callback(null, {ok: true, err: []})
+  testFunction: function(context, callback) {
+    callback(null, { success: true })
   }
 }
